a8ce9a53f0e0c176a576a512117409a85045ec30
sohseyedi <sohseyedi@gmail.com>
2023-11-18 14:49:17 +0330

    product list

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,17 @@
+import { productList } from "../../utils/dataList";
 import Header from "./Header";
+import ProductList from "./ProductList";
 
 const Home = () => {
-
-
   return (
     <section className="max-w-7xl mx-auto py-5">
-      <Header/>
-      <hr className="my-4"/>
+      <Header />
+      <hr className="my-4" />
+      <div className="flex items-center gap-x-6 flex-wrap gap-y-5">
+        {productList.map((product) => (
+          <ProductList key={product.id} product={product} />
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/components/home/ProductList.tsx
@@ -0,0 +1,57 @@
+import { ProductsListType } from "../../types/type";
+import * as RiIcon from "react-icons/ri";
+import { useCartStore } from './../../store/useCart';
+
+const ProductList = ({ product }: { product: ProductsListType }) => {
+
+    const {addItemToCart} = useCartStore()
+
+  return (
+    <div
+      key={product.id}
+      className="lg:w-[32%] md:w-[45%] w-[95%] p-2 lg:mx-0 mx-auto border border-gray-300 shadow rounded-xl"
+    >
+      <div
+        className="p-1 w-full h-[150px] py-2 rounded-xl"
+        style={{
+          background: `linear-gradient(to bottom , ${product.oneColor} , ${product.twoColor})`,
+        }}
+      >
+        <img
+          src={product.img}
+          alt={product.name}
+          className="w-full h-full object-contain rounded-xl"
+        />
+      </div>
+      <h2 className="my-3 text-lg font-semibold">
+        {product.name}-{" "}
+        <span>{product.gender === "men" ? "مردانه" : "زنانه"}</span>
+      </h2>
+      <h6 className="my-2 text-lg ">قیمت : {product.price}</h6>
+
+      <div className="my-2 flex items-center gap-x-2 text-lg font-semibold">
+        سایز :
+        {product.size.map((s) => (
+          <p className="w-[30px] text-base font-normal h-[30px] text-white m-0 rounded-full bg-blue-500 flex items-center justify-center">
+            {s}
+          </p>
+        ))}
+      </div>
+      <div className="flex items-center justify-between">
+        <button onClick={() => addItemToCart(product)} className="h-[45px] w-[45%] font-semibold text-gray-200 rounded-xl border-none bg-indigo-600">
+          افزودن به سبد خرید
+        </button>
+        <div className="flex items-center justify-center gap-x-4  text-indigo-600 ">
+          <span className="cursor-pointer shadow hover:scale-110 transition-all duration-300">
+            <RiIcon.RiHeart2Line size={26} />
+          </span>
+          <span className="ml-3 cursor-pointer shadow hover:scale-110 transition-all duration-300">
+            <RiIcon.RiBookmarkLine size={26} />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
