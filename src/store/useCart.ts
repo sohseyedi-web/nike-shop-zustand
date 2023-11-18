9658d36117dab3e33d111abb40c9b4d38d65428d
sohseyedi <sohseyedi@gmail.com>
2023-11-18 10:35:09 +0330

    add persist

--- src/store/useCart.ts
+++ src/store/useCart.ts
@@ -1,26 +1,35 @@
 import { create } from "zustand";
 import { CartProduct, ProductsListType } from "../types/type";
+import { persist } from "zustand/middleware";
 
 interface CartState {
   cartItems: CartProduct[];
   addItemToCart: (item: ProductsListType) => void;
 }
 
-export const useCartStore = create<CartState>((set, get) => ({
-  cartItems: [],
-  addItemToCart: (item) => {
-    const itemExists = get().cartItems.find(
-      (cartItem) => cartItem.id === item.id
-    );
+export const useCartStore = create(
+  persist<CartState>(
+    (set, get) => ({
+      cartItems: [],
 
-    if (itemExists) {
-      if (typeof itemExists.quantity === "number") {
-        itemExists.quantity++;
-      }
+      addItemToCart: (item) => {
+        const itemExists = get().cartItems.find(
+          (cartItem) => cartItem.id === item.id
+        );
 
-      set({ cartItems: [...get().cartItems] });
-    } else {
-      set({ cartItems: [...get().cartItems, { ...item, quantity: 1 }] });
+        if (itemExists) {
+          if (typeof itemExists.quantity === "number") {
+            itemExists.quantity++;
+          }
+
+          set({ cartItems: [...get().cartItems] });
+        } else {
+          set({ cartItems: [...get().cartItems, { ...item, quantity: 1 }] });
+        }
+      },
+    }),
+    {
+      name: "cart-items",
     }
-  },
-}));
+  )
+);
