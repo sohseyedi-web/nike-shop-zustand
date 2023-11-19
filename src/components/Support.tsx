551d1470e296da0358a9f84f64225f427e3f6576
sohseyedi <sohseyedi@gmail.com>
2023-11-19 10:25:51 +0330

    validation form

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -1,9 +1,26 @@
 import * as RiIcon from "react-icons/ri";
 import FormCard from "./common/FormCard";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type InputType = {
+  fullName: string;
+  email: string;
+  options: string;
+  message?: string;
+};
 
 const Support = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isDirty },
+  } = useForm<InputType>();
+
+  const onSubmit: SubmitHandler<InputType> = (data) => console.log(data);
+
   return (
-    <FormCard>
+    <FormCard onSubmit={handleSubmit(onSubmit)}>
       <div className="flex items-center gap-x-2 text-indigo-600 ">
         <RiIcon.RiCustomerService2Line size={30} />
         <h6 className="text-lg font-semibold">پشتیبانی</h6>
@@ -14,9 +31,11 @@ const Support = () => {
         </label>
         <input
           type="text"
+          {...register("fullName", { required: true })}
           placeholder="سهیل سیدی"
           className="w-full h-[45px] px-1 rounded-xl shadow-sm transition-all duration-200 focus:border-indigo-400 outline-none border-2"
         />
+        {errors.fullName && <span className="text-red-500 my-1">نام الزامی است</span>}
       </div>
       <div className="w-full">
         <label className="mb-2 font-semibold" htmlFor="">
@@ -24,20 +43,26 @@ const Support = () => {
         </label>
         <input
           dir="ltr"
+          {...register("email", { required: true })}
           type="email"
           placeholder="example@gmail.com"
           className="w-full h-[45px] px-1 rounded-xl shadow-sm transition-all duration-200 focus:border-indigo-400 outline-none border-2"
         />
+        {errors.email?.type === "required" && <span className="text-red-500 my-1">ایمیل الزامی است</span>}
+        {errors.email?.type === "pattern" && <span className="text-red-500 my-1">ایمیل معتبر نیست</span>}
       </div>
       <div className="w-full">
         <label className="mb-2 font-semibold" htmlFor="">
           ارتباط با{" "}
         </label>
 
-        <select className="w-full px-1 h-[45px] rounded-xl shadow-sm transition-all duration-200 focus:border-indigo-400 outline-none border-2">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+        <select
+          {...register("options", { required: true })}
+          className="w-full px-1 h-[45px] rounded-xl shadow-sm transition-all duration-200 focus:border-indigo-400 outline-none border-2"
+        >
+          <option value="khadamat">خدمات</option>
+          <option value="sell">فروش</option>
+          <option value="accounting">حسابداری</option>
         </select>
       </div>
       <div className="w-full">
@@ -45,11 +70,15 @@ const Support = () => {
           توضیحات
         </label>
         <textarea
+          {...register("message")}
           placeholder="در صورت تمایل توضیحاتی درباره درخواست خود بنویسید"
           className="w-full p-1 resize-none h-[145px] rounded-xl shadow-sm transition-all duration-200 focus:border-indigo-400 outline-none border-2"
         />
       </div>
-      <button className="border w-full h-[45px] rounded-xl bg-green-500 text-white text-lg font-semibold transition-all duration-300 hover:bg-green-700">
+      <button
+        disabled={!isDirty}
+        className="border w-full h-[45px] rounded-xl bg-green-500 text-white text-lg font-semibold transition-all duration-300 hover:bg-green-700"
+      >
         ثبت درخواست
       </button>
     </FormCard>
--- src/components/common/FormCard.tsx
+++ src/components/common/FormCard.tsx
@@ -1,12 +1,19 @@
+type PropsType = {
+  children: React.ReactNode;
+  onSubmit: () => void;
+};
 
-const FormCard = ({children} : {children : React.ReactNode}) => {
+const FormCard = ({ children, onSubmit }: PropsType) => {
   return (
     <section className="flex items-center justify-center h-full">
-      <form className="space-y-4 lg:w-[35%] p-3 md:w-[75%] w-[95%] shadow rounded-xl border border-gray-200 flex items-center justify-center flex-col">
+      <form
+        onSubmit={onSubmit}
+        className="space-y-4 lg:w-[35%] p-3 md:w-[75%] w-[95%] shadow rounded-xl border border-gray-200 flex items-center justify-center flex-col"
+      >
         {children}
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default FormCard
+export default FormCard;
